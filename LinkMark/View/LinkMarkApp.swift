48d6715315d77b55d1c0a04f5d3b70fca93aa759
amefure <shachi10969@gmail.com>
2024-01-04 23:42:49 +0900

    初回プロジェクト構築

---
+++ LinkMark/View/LinkMarkApp.swift
@@ -0,0 +1,32 @@
+//
+//  LinkMarkApp.swift
+//  LinkMark
+//
+//  Created by t&a on 2024/01/04.
+//
+
+import SwiftUI
+import FirebaseCore
+import GoogleMobileAds
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
+
+@main
+struct LinkMarkApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
