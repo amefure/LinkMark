90cb2267deb0fd1c283f8d4dc52b3a932a8e296d
amefure <shachi10969@gmail.com>
2024-01-20 16:58:11 +0900

    デモデータの追加

--- LinkMark/Model/Category+CoreDataClass.swift
+++ LinkMark/Model/Category+CoreDataClass.swift
@@ -12,18 +12,48 @@ import CoreData
 @objc(Category)
 public class Category: NSManagedObject {
 
-}
-
-
-extension Category {
-   
     // 値がnilの場合のデフォルト値定義
     public var wrappedId: UUID { id ?? UUID() }
     public var wrappedName: String { name ?? "" }
     public var wrappedColor: String { color ?? CategoryColor.red.rawValue }
     public var wrappedLocators: NSSet { locator ?? NSSet() }
     public var wrappedOrder: Int { Int(order) }
-    
+}
 
+
+extension Category {
+    
+    /// デモプレビュー用インスタンス生成メソッド
+    static func new(name: String, color: String, order: Int) -> Category {
+        let entity: Category = CoreDataRepository.entity()
+        entity.id = UUID()
+        entity.name = name
+        entity.color = color
+        entity.order = Int16(order)
+        return entity
+    }
+   
+    /// デモリスト
+    static var demoCategorys : [Category] {
+        
+        var categorys: [Category]  = []
+        
+        let category1 = Category.new(name: "料理", color: "red", order: 0)
+        categorys.append(category1)
+        
+        let category2 = Category.new(name: "趣味", color: "yellow", order: 1)
+        categorys.append(category2)
+        
+        let category3 = Category.new(name: "勉強", color: "blue", order: 2)
+        categorys.append(category3)
+        
+        let category4 = Category.new(name: "子供", color: "green", order: 3)
+        categorys.append(category4)
+        
+        let category5 = Category.new(name: "テニス", color: "purple", order: 4)
+        categorys.append(category5)
+        
+        return categorys
+    }
 }
 
--- LinkMark/Model/Locator+CoreDataClass.swift
+++ LinkMark/Model/Locator+CoreDataClass.swift
@@ -18,6 +18,43 @@ public class Locator: NSManagedObject {
     public var wrappedMemo: String { memo ?? "" }
     public var wrappedCreatedAt: Date { createdAt ?? Date() }
     public var wrappedOrder: Int { Int(order) }
+}
+
+extension Locator {
     
+    /// デモプレビュー用インスタンス生成メソッド
+    static func new(title: String, url: URL, memo: String, order: Int) -> Locator {
+        let entity: Locator = CoreDataRepository.entity()
+        entity.id = UUID()
+        entity.title = title
+        entity.url = url
+        entity.memo = memo
+        entity.order = Int16(order)
+        entity.createdAt = Date()
+        return entity
+    }
+   
+    /// デモリスト
+    static var demoLocators : [Locator] {
+        
+        var locators: [Locator]  = []
+        
+        let locator1 = Locator.new(title: "肉じゃがの作り方(3人前)", url: URL(string: "https://tech.amefure.com/")!, memo: "", order: 0)
+        locators.append(locator1)
+        
+        let locator2 = Locator.new(title: "麻婆豆腐レシピ", url: URL(string: "https://tech.amefure.com/")!, memo: "豆板醤が必要", order: 1)
+        locators.append(locator2)
+        
+        let locator3 = Locator.new(title: "作り置きまとめサイト", url: URL(string: "https://tech.amefure.com/")!, memo: "", order: 2)
+        locators.append(locator3)
+        
+        let locator4 = Locator.new(title: "野菜の選び方特集", url: URL(string: "https://tech.amefure.com/")!, memo: "・ほうれん草は茹でる", order: 3)
+        locators.append(locator4)
+        
+        let locator5 = Locator.new(title: "トマトを使ったレシピ", url: URL(string: "https://tech.amefure.com/")!, memo: "", order: 4)
+        locators.append(locator5)
+        
+        return locators
+    }
 }
 
